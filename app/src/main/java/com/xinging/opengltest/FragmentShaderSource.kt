6c693b61c6c54ea143882d72bf0c0ae794954583
william <460706836@qq.com>
2023-02-21 07:05:22 +0800

    add 'precision mediump float' to fragment source to fix compile error on some android devices

--- app/src/main/java/com/xinging/opengltest/FragmentShaderSource.kt
+++ app/src/main/java/com/xinging/opengltest/FragmentShaderSource.kt
@@ -4,6 +4,7 @@ class FragmentShaderSource {
     companion object {
         val fragmentShaderSource = """
             #version 300 es
+            precision mediump float;
             out vec4 FragColor;
             void main()
             {
